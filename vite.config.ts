91a0b1bd31da72f699bfbac626e5ab6809847a61
Ashish Verma <ashuverma9499@gmail.com>
2026-02-24 13:22:39 +0530

    repo ame change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/tree--view/" : "/",
+  base: mode === "production" ? "/tree-explorer/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
